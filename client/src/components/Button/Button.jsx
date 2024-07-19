4d44364b845b50ece6005c09b03cfb361203a6af
Anurag Raushan <anuragraushan373@gmail.com>
2024-07-20 01:41:58 +0530

    done

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import "./button.css";
 
 const Button = ({ children }) => {
-  const showToast = () => {
-    alert("Button Clicked");
-  };
-  return (
-    <button className="custom-button" onClick={showToast()}>
-      {children}
-    </button>
-  );
+  return <button className="custom-button">{children}</button>;
 };
 
 export default Button;
